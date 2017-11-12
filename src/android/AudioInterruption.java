b7ddd4f28452d0dd64b25cdff48603da66b89340
zzzaki <techokill@gmail.com>
2017-11-12 13:33:05 +0700

    updated plugin for android

--- src/android/AudioInterruption.java
+++ src/android/AudioInterruption.java
@@ -3,76 +3,63 @@ package com.wingood.cordova.audiointerruption;
 import org.apache.cordova.*;
 
 import android.content.Context;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
+import android.media.AudioManager;
+import android.media.AudioManager.OnAudioFocusChangeListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class AudioInterruption extends CordovaPlugin {
-    CallStateListener listener;
+    AudioStateListener listener;
+    AudioManager audioManager;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("addListener")) {
-            prepareListener();
-            listener.setCallbackContext(callbackContext);
+            prepareListener(callbackContext);
             return true;
         } else {
             return false;
         }
     }
 
-    private void prepareListener() {
+    private void prepareListener(CallbackContext callbackContext) {
         if (listener == null) {
-            listener = new CallStateListener();
-            TelephonyManager TelephonyMgr = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-            TelephonyMgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+            listener = new AudioStateListener();
+            listener.setCallbackContext(callbackContext);
+
+            Context context = this.cordova.getActivity().getApplicationContext();
+            audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            // TODO add error handler
+            audioManager.requestAudioFocus(listener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
         }
     }
 }
 
-class CallStateListener extends PhoneStateListener {
+class AudioStateListener implements OnAudioFocusChangeListener {
     private CallbackContext callbackContext;
-    private int prevState;
 
     public void setCallbackContext(CallbackContext callbackContext) {
         this.callbackContext = callbackContext;
     }
 
-    public void onCallStateChanged(int state, String incomingNumber) {
-        super.onCallStateChanged(state, incomingNumber);
-
-        if (callbackContext == null) return;
-
-        String msg = "";
-
-        switch (state) {
-            case TelephonyManager.CALL_STATE_IDLE:
-                if ((prevState == TelephonyManager.CALL_STATE_OFFHOOK)) {
-                    // A call has now ended
-                    prevState = state;
-                    msg = "ENDED";
-                } else if ((prevState == TelephonyManager.CALL_STATE_RINGING)) {
-                    // Rejected or Missed call
-                    prevState = state;
-                    msg = "ENDED";
-                }
-            break;
-
-            case TelephonyManager.CALL_STATE_OFFHOOK:
-                msg = "OFFHOOK";
-                prevState = state;
-            break;
-
-            case TelephonyManager.CALL_STATE_RINGING:
-                msg = "RINGING";
-                prevState = state;
-            break;
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+        String status = "";
+        switch (focusChange) {
+            case AudioManager.AUDIOFOCUS_LOSS:
+                status = "INTERRUPTION_BEGIN";
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                status = "INTERRUPTION_BEGIN";
+                break;
+            case AudioManager.AUDIOFOCUS_GAIN:
+                status = "INTERRUPTION_ENDED";
+                break;
         }
 
-        if(msg != null && !msg.isEmpty()) {
-            PluginResult result = new PluginResult(PluginResult.Status.OK, msg);
+        if(status != null && !status.isEmpty()) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, status);
             result.setKeepCallback(true);
 
             callbackContext.sendPluginResult(result);
