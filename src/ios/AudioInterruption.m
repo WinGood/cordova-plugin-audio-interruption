a9c292f874396f858615e5a06f70c8274d267642
zzzaki <techokill@gmail.com>
2017-11-12 12:01:19 +0700

    plugin rename

---
+++ src/ios/AudioInterruption.m
@@ -0,0 +1,36 @@
+#import "PhoneCallInterruption.h"
+#import <CoreTelephony/CTCallCenter.h>
+#import <CoreTelephony/CTCall.h>
+
+@implementation PhoneCallInterruption
+
+- (void) pluginInitialize
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAudioSessionEvent:) name:AVAudioSessionInterruptionNotification object:nil];
+}
+
+- (void) onCall:(CDVInvokedUrlCommand*)command
+{
+    successCallbackID = command.callbackId;
+}
+
+// Send any data back to JS env through subscribe callback
+- (void) sendStatusNameInJS: (NSString*) status {
+    plresult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:status];
+    [plresult setKeepCallbackAsBool:YES];
+    [self.commandDelegate sendPluginResult:plresult callbackId:successCallbackID];
+}
+
+- (void) onAudioSessionEvent: (NSNotification *) notification
+{
+    if ([notification.name isEqualToString:AVAudioSessionInterruptionNotification]) {
+        // Check to see if it was a Begin interruption
+        if ([[notification.userInfo valueForKey:AVAudioSessionInterruptionTypeKey] isEqualToNumber:[NSNumber numberWithInt:AVAudioSessionInterruptionTypeBegan]]) {
+            [self sendStatusNameInJS:@"RINGING"];
+        } else {
+            [self sendStatusNameInJS:@"ENDED"];
+        }
+    }
+}
+
+@end
