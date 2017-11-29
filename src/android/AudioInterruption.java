3dad26fe33db1525d35c09411e65c7be38d165c3
zzzaki <techokill@gmail.com>
2017-11-29 16:32:27 +0700

    updated plugin for android

--- src/android/AudioInterruption.java
+++ src/android/AudioInterruption.java
@@ -1,71 +1,85 @@
 package com.wingood.cordova.audiointerruption;
 
 import org.apache.cordova.*;
-
 import android.content.Context;
 import android.media.AudioManager;
-import android.media.AudioManager.OnAudioFocusChangeListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class AudioInterruption extends CordovaPlugin {
-    AudioStateListener listener;
-    CallInterruption listener2;
-    AudioManager audioManager;
+    private CallbackContext callbackContext;
+    private Context mContext;
+    private boolean mAudioFocusGranted = false;
+    private boolean mListenerRegistered = false;
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         if (action.equals("addListener")) {
-            prepareListener(callbackContext);
+            addListener(callbackContext);
             return true;
         } else {
             return false;
         }
     }
 
-    private void prepareListener(CallbackContext callbackContext) {
-        if (listener == null) {
-            listener2 = new CallInterruption();
-            listener2.setCallbackContext(callbackContext);
-            // listener = new AudioStateListener();
-            // listener.setCallbackContext(callbackContext);
+    private void addListener(CallbackContext callbackContext) {
+        if (!mListenerRegistered) {
+            Context context = this.cordova.getActivity().getApplicationContext();
 
-            // Context context = this.cordova.getActivity().getApplicationContext();
-            // audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-            // TODO add error handler
-            // audioManager.requestAudioFocus(listener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+            this.callbackContext = callbackContext;
+            this.mContext = context;
+
+            mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    switch (focusChange) {
+                        case AudioManager.AUDIOFOCUS_LOSS:
+                            sendStatusInJS("INTERRUPTION_BEGIN");
+                            break;
+                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                            sendStatusInJS("INTERRUPTION_BEGIN");
+                            break;
+                        case AudioManager.AUDIOFOCUS_GAIN:
+                            sendStatusInJS("INTERRUPTION_ENDED");
+                            break;
+                        case AudioManager.AUDIOFOCUS_REQUEST_FAILED:
+                            sendErrorInJS("AUDIOFOCUS_REQUEST_FAILED");
+                            break;
+                    }
+                }
+            };
+
+            this.mListenerRegistered = true;
+            this.requestAudioFocus();
         }
     }
-}
 
-class AudioStateListener implements OnAudioFocusChangeListener {
-    private CallbackContext callbackContext;
-
-    public void setCallbackContext(CallbackContext callbackContext) {
-        this.callbackContext = callbackContext;
+    private void requestAudioFocus() {
+        if (!mAudioFocusGranted) {
+            // TODO add support for Android O, AudioFocusRequest.Builder
+            AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+            int result = am.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                mAudioFocusGranted = true;
+            } else {
+                sendErrorInJS("AUDIOFOCUS_REQUEST_FAILED");
+            }
+        }
     }
 
-    @Override
-    public void onAudioFocusChange(int focusChange) {
-        String status = "";
-        switch (focusChange) {
-            case AudioManager.AUDIOFOCUS_LOSS:
-                status = "INTERRUPTION_BEGIN";
-                break;
-            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                status = "INTERRUPTION_BEGIN";
-                break;
-            case AudioManager.AUDIOFOCUS_GAIN:
-                status = "INTERRUPTION_ENDED";
-                break;
-        }
+    private void sendStatusInJS(String status) {
+        if (this.callbackContext == null) return;
+        PluginResult result = new PluginResult(PluginResult.Status.OK, status);
+        result.setKeepCallback(true);
+        this.callbackContext.sendPluginResult(result);
+    }
 
-        if(status != null && !status.isEmpty()) {
-            PluginResult result = new PluginResult(PluginResult.Status.OK, status);
-            result.setKeepCallback(true);
-
-            callbackContext.sendPluginResult(result);
-        }
+    private void sendErrorInJS(String error) {
+        if (this.callbackContext == null) return;
+        PluginResult result = new PluginResult(PluginResult.Status.ERROR, error);
+        result.setKeepCallback(true);
+        this.callbackContext.sendPluginResult(result);
     }
 }
