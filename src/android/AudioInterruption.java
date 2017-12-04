6b6cdca11503469f53a7568def7d7d5cf35a81a9
zzzaki <techokill@gmail.com>
2017-12-04 17:43:36 +0700

    updated plugin for android

--- src/android/AudioInterruption.java
+++ src/android/AudioInterruption.java
@@ -2,17 +2,19 @@ package com.wingood.cordova.audiointerruption;
 
 import org.apache.cordova.*;
 import android.content.Context;
-import android.media.AudioManager;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.Date;
+
 public class AudioInterruption extends CordovaPlugin {
     private CallbackContext callbackContext;
     private Context mContext;
-    private boolean mAudioFocusGranted = false;
     private boolean mListenerRegistered = false;
-    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener;
+    private PhoneCallStateListener listener;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -31,54 +33,69 @@ public class AudioInterruption extends CordovaPlugin {
             this.callbackContext = callbackContext;
             this.mContext = context;
 
-            mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-                @Override
-                public void onAudioFocusChange(int focusChange) {
-                    switch (focusChange) {
-                        case AudioManager.AUDIOFOCUS_LOSS:
-                            sendStatusInJS("INTERRUPTION_BEGIN");
-                            break;
-                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                            sendStatusInJS("INTERRUPTION_BEGIN");
-                            break;
-                        case AudioManager.AUDIOFOCUS_GAIN:
-                            sendStatusInJS("INTERRUPTION_ENDED");
-                            break;
-                        case AudioManager.AUDIOFOCUS_REQUEST_FAILED:
-                            sendErrorInJS("AUDIOFOCUS_REQUEST_FAILED");
-                            break;
-                    }
-                }
-            };
-
+            listener = new PhoneCallStateListener();
+            listener.setCallbackContext(this.callbackContext);
+            TelephonyManager telephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
             this.mListenerRegistered = true;
-            this.requestAudioFocus();
+        }
+    }
+}
+
+
+class PhoneCallStateListener extends PhoneStateListener {
+    private CallbackContext callbackContext;
+    private static int lastState = TelephonyManager.CALL_STATE_IDLE;
+    private static Date callStartTime;
+    private static boolean isIncoming;
+    private static String savedNumber;
+
+    public void setCallbackContext(CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+    }
+
+    public void onCallStateChanged(int state, String number) {
+        if(lastState == state){
+            return;
+        }
+
+        String status = "";
+
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                isIncoming = true;
+                callStartTime = new Date();
+                savedNumber = number;
+                status = "INTERRUPTION_BEGIN"; // incoming call
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                if (lastState != TelephonyManager.CALL_STATE_RINGING) {
+                    isIncoming = false;
+                    callStartTime = new Date();
+                    status = "INTERRUPTION_BEGIN"; // outgoing call
+                }
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                if (lastState == TelephonyManager.CALL_STATE_RINGING) {
+                    status = "INTERRUPTION_ENDED"; // missed call
+                } else if (isIncoming) {
+                    status = "INTERRUPTION_ENDED"; // incoming ended
+                } else {
+                    status = "INTERRUPTION_ENDED"; // outgoing ended
+                }
+                break;
+        }
+
+        lastState = state;
+
+        if (!status.isEmpty()) {
+            this.sendStateInJS(status);
         }
     }
 
-    private void requestAudioFocus() {
-        if (!mAudioFocusGranted) {
-            // TODO add support for Android O, AudioFocusRequest.Builder
-            AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
-            int result = am.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-            if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                mAudioFocusGranted = true;
-            } else {
-                sendErrorInJS("AUDIOFOCUS_REQUEST_FAILED");
-            }
-        }
-    }
-
-    private void sendStatusInJS(String status) {
+    private void sendStateInJS(String status) {
         if (this.callbackContext == null) return;
         PluginResult result = new PluginResult(PluginResult.Status.OK, status);
-        result.setKeepCallback(true);
-        this.callbackContext.sendPluginResult(result);
-    }
-
-    private void sendErrorInJS(String error) {
-        if (this.callbackContext == null) return;
-        PluginResult result = new PluginResult(PluginResult.Status.ERROR, error);
         result.setKeepCallback(true);
         this.callbackContext.sendPluginResult(result);
     }
